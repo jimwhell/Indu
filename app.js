2c8e4ef34f23195d284191c2a652d7408a316ba2
jimwell <jimwelllmanalo@gmail.com>
2025-03-01 22:33:47 +0800

    final commit

--- app.js
+++ app.js
@@ -1,59 +1,54 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const bodyParser = require('body-parser');
-const admin = require('firebase-admin');
-
-const credentials = require('./serviceAccountKey.json');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
 
 // const csrfMiddleware = csrf({cookie: true});
 
-var authRouter = require('./routes/auth');
-const adminAuthRouter = require('./routes/adminAuth');
-const pageRoutes = require('./routes/pageRoutes');
-const productsRouter = require('./routes/products');
-const categoriesRouter = require('./routes/categories');
-const cartRouter = require('./routes/cart');
-const ordersRouter = require('./routes/orders');
-const customersRouter = require('./routes/customers');
-const session = require('express-session');
+var authRouter = require("./routes/auth");
+const adminAuthRouter = require("./routes/adminAuth");
+const pageRoutes = require("./routes/pageRoutes");
+const productsRouter = require("./routes/products");
+const categoriesRouter = require("./routes/categories");
+const cartRouter = require("./routes/cart");
+const ordersRouter = require("./routes/orders");
+const customersRouter = require("./routes/customers");
+const session = require("express-session");
 
 var app = express();
 
-
-app.use(session({
-    secret: 'your-secret-key',
+app.use(
+  session({
+    secret: "your-secret-key",
     resave: false,
     saveUninitialized: true,
-  }));
-
-
+  })
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 // app.use(csrfMiddleware);
 
 // app.use('/', indexRouter);
-app.use('/auth', authRouter);
-app.use('/', pageRoutes);
-app.use('/api/products/', productsRouter);
-app.use('/api/categories/', categoriesRouter);
-app.use('/api/cart/', cartRouter);
-app.use('/api/orders/', ordersRouter);
-app.use('/customers', customersRouter);
-app.use('/adminAuth', adminAuthRouter);
-
+app.use("/auth", authRouter);
+app.use("/", pageRoutes);
+app.use("/api/products/", productsRouter);
+app.use("/api/categories/", categoriesRouter);
+app.use("/api/cart/", cartRouter);
+app.use("/api/orders/", ordersRouter);
+app.use("/customers", customersRouter);
+app.use("/adminAuth", adminAuthRouter);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
